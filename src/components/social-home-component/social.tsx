e0b894aa5df13130e90100b9fa0f5325c556d98a
payep <l.payep@groupeubique.com>
2023-01-17 09:20:50 +0100

    yarn changes

--- src/components/social-home-component/social.tsx
+++ src/components/social-home-component/social.tsx
@@ -1,14 +1,17 @@
 import React, { FunctionComponent } from 'react';
 // import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { HiMail } from "react-icons/hi";
+// import { HiMail } from "react-icons/hi";
 import './social.scss';
 
 const Social: FunctionComponent = () => {
     return(
         <div className="container">
             {/* <span><FaLinkedinIn/></span>
-            <span><FaGithub/></span> */}
-            <span><HiMail/></span>
+            <span><FaGithub/></span>
+            <span><HiMail/></span> */}
+            <span>Lin</span>
+            <span>git</span>
+            <span>mai</span>
         </div>
     )
 }
